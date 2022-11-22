042c0babc9ad108731362ce33e8bc3563ffb60b3
Mayra Trujillo <115262947+DNA-Bioboss@users.noreply.github.com>
2022-11-22 17:22:24 +0000

    Add John's favorite color

--- TestMXT.R
+++ TestMXT.R
@@ -1,6 +1,9 @@
-<<<<<<< HEAD
 "This is RStudio's line"
-=======
-"This is GitHub's line"
->>>>>>> 26e8fb57bd8bbe201b7766e29d87f22e807f6971
 "Hello from GitHub"
+
+"This is how a conflict is resolved."
+
+"Mayra's favorite color is:"
+"Purple"
+
+"What is John's favorite color?"
