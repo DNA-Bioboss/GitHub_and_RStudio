89a6abf9bccd9cc5346207668856a7420364f572
Mayra Trujillo <115262947+DNA-Bioboss@users.noreply.github.com>
2022-11-22 17:47:22 +0000

    What is John's dog named?

--- TestMXT.R
+++ TestMXT.R
@@ -8,3 +8,8 @@
 
 "What is John's favorite color?"
 "Blue or Green or Brown equally tie for my favorite"
+
+"My family's dog is named Bubba."
+
+"What is John's dog named?"
+I need your name to appear more than once in the history. Delete this line and enter pet's name please.'
